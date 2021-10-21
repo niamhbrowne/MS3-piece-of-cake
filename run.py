577b9f673f121e4d6b7d256d6da2f797b639a11f
niamhbrowne <niamh_browne@icloud.com>
2021-10-21 11:55:00 +0000

    added working nav bar to all pages

--- run.py
+++ run.py
@@ -16,13 +16,13 @@ def about():
 
 
 @app.route("/shop")
-def about():
+def shop():
     return render_template("shop.html")
 
 
-@app.route("/contactus")
-def about():
-    return render_template(contact.html)
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
