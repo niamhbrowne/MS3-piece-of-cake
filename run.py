f5413ad68b7f49a073ec9a33798ecdfd0f6a00be
niamhbrowne <niamh_browne@icloud.com>
2021-10-21 11:41:03 +0000

    changed the url links for nav bar

--- run.py
+++ run.py
@@ -10,6 +10,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/shop")
+def about():
+    return render_template("shop.html")
+
+
+@app.route("/contactus")
+def about():
+    return render_template(contact.html)
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
